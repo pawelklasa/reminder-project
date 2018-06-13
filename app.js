3e47810b8916cb2efc229eb9c2cfde2b30c0fbf4
Pawel Klasa <pawel.klasa@gmail.com>
2018-06-13 13:28:50 +0100

    white space

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Umodularized 
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.datepicker');
   var instances = M.Datepicker.init(elems, );
